8f2244cfed6f4467d7758203df31c43efc33ea17
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2022-02-20 14:05:21 +0600

    fix(redux-store, userComponents): add space

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import s from './style.module.css'
+import React from "react";
+import s from "./style.module.css";
 import Button from "../Button";
 import axios from "axios";
-import imgMocUser from '../../assets/users.png'
+import imgMocUser from "../../assets/users.png";
 
 const Users = (props) => {
-
   if (props.users.length === 0) {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users')
-      .then(response => {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
         props.setUsers(response.data.items);
       });
   }
-
 
   return (
     <div className={s.users}>
@@ -23,18 +22,28 @@ const Users = (props) => {
             <div className={s.usersWrapper} key={u.id}>
               <div>
                 <div>
-                  <img className={s.userPhoto} src={u.photos.small != null ? u.photos.small : imgMocUser} alt="avatar"/>
+                  <img
+                    className={s.userPhoto}
+                    src={u.photos.small != null ? u.photos.small : imgMocUser}
+                    alt="avatar"
+                  />
                 </div>
                 <div className={s.usersBtn}>
-                  {
-                    u.followed
-                      ? <Button onClick={() => {
-                        props.unfollow(u.id)
-                      }} text="UnFollow"/>
-                      : <Button onClick={() => {
-                        props.follow(u.id)
-                      }} text="Follow"/>
-                  }
+                  {u.followed ? (
+                    <Button
+                      onClick={() => {
+                        props.unfollow(u.id);
+                      }}
+                      text="UnFollow"
+                    />
+                  ) : (
+                    <Button
+                      onClick={() => {
+                        props.follow(u.id);
+                      }}
+                      text="Follow"
+                    />
+                  )}
                 </div>
               </div>
               <div className={s.bio}>
@@ -43,19 +52,24 @@ const Users = (props) => {
                     <div>
                       <span>fullname:</span> {u.name}
                     </div>
-                    <div className={s.usersInfoStatus}><span>status:</span> {u.status}</div>
+                    <div className={s.usersInfoStatus}>
+                      <span>status:</span> {u.status}
+                    </div>
                   </div>
                 </div>
                 <div className={s.usersLocation}>
-                  <div><span>country:</span></div>
+                  <div>
+                    <span>country:</span>
+                  </div>
                   {/*<div><span>country</span> {u.location.country}</div>*/}
-                  <div className={s.usersCity}><span>city:</span></div>
+                  <div className={s.usersCity}>
+                    <span>city:</span>
+                  </div>
                   {/*<div className={s.usersCity}><span>city</span> {u.location.city}</div>*/}
                 </div>
               </div>
             </div>
-          ))
-        }
+          ))}
       </div>
     </div>
   );
