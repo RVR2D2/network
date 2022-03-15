07bef02181832138856e2f7a6c4abb85100cc8df
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2022-03-15 23:48:38 +0600

    feat( ProfileStatus.jsx) add componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -26,6 +26,12 @@ class ProfileStatus extends React.Component {
     });
   };
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({ status: this.props.status });
+    }
+  }
+
   render() {
     return (
       <>
