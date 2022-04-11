13131bc858acc6fd3233dcd89e327df7ea179089
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2022-04-11 22:41:56 +0600

    feat(validators, Button, FormsControl, Login, api, reducers): add type to redux-form and create custom jeneric

--- src/redux/reducers/auth-reducer.ts
+++ src/redux/reducers/auth-reducer.ts
@@ -1,4 +1,9 @@
-import { authApi, securityApi } from "../../api/api";
+import {
+  authApi,
+  ResultCodesEnum,
+  ResultCodesWithCaptcha,
+  securityApi,
+} from "../../api/api";
 import { stopSubmit } from "redux-form";
 
 const SET_USER_DATA = "auth/SET_USER_DATA";
@@ -73,9 +78,9 @@ export const getCaptchaUrl = (captchaUrl: string): getCaptchaUrlActionType => ({
 });
 
 export const authMeThunk = () => async (dispatch: any) => {
-  let response = await authApi.authMe();
-  if (response.data.resultCode === 0) {
-    let { id, email, login } = response.data.data;
+  let meData = await authApi.authMe();
+  if (meData.resultCode === ResultCodesEnum.Success) {
+    let { id, email, login } = meData.data;
     dispatch(setAuthUserData(id, email, login, true));
   }
 };
@@ -83,18 +88,16 @@ export const authMeThunk = () => async (dispatch: any) => {
 export const login =
   (email: string, password: string, rememberMe: boolean, captcha: null) =>
   async (dispatch: any) => {
-    let response = await authApi.login(email, password, rememberMe, captcha);
-    if (response.data.resultCode === 0) {
+    let loginData = await authApi.login(email, password, rememberMe, captcha);
+    if (loginData.resultCode === ResultCodesEnum.Success) {
       dispatch(authMeThunk());
     } else {
-      if (response.data.resultCode === 10) {
+      if (loginData.resultCode === ResultCodesWithCaptcha.CaptchaIsRequired) {
         dispatch(getCaptchaThunk());
       }
 
       let message =
-        response.data.messages.length > 0
-          ? response.data.messages[0]
-          : "Some error";
+        loginData.messages.length > 0 ? loginData.messages[0] : "Some error";
       dispatch(stopSubmit("login", { _error: message }));
     }
   };
@@ -107,7 +110,7 @@ export const getCaptchaThunk = () => async (dispatch: any) => {
 
 export const logout = () => async (dispatch: any) => {
   let response = await authApi.logout();
-  if (response.data.resultCode === 0) {
+  if (response.data.resultCode === ResultCodesEnum.Success) {
     dispatch(setAuthUserData(null, null, null, false));
   }
 };
