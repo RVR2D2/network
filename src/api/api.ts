cfb4a829b2e766b500ba30e1d58e26e93cdcb860
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2022-04-11 22:41:56 +0600

    feat(api.ts, auth-reducer.ts): add type for response, add enum

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,36 +7,36 @@ const instance = axios.create({
 });
 
 export const usersApi = {
-  getUsers(currentPage, pageSize) {
+  getUsers(currentPage: number, pageSize: number) {
     return instance
       .get(`users?page=${currentPage}&count=${pageSize}`)
       .then((response) => {
         return response.data;
       });
   },
-  follow(userId) {
+  follow(userId: number) {
     return instance.post(`follow/${userId}`);
   },
-  unfollow(userId) {
+  unfollow(userId: number) {
     return instance.delete(`follow/${userId}`);
   },
-  profile(userId) {
+  profile(userId: number) {
     console.warn("Obsolute method, Please profileApi object.");
     return profileApi.profile(userId);
   },
 };
 
 export const profileApi = {
-  profile(userId) {
+  profile(userId: number) {
     return instance.get(`profile/${userId}`);
   },
-  getStatus(userId) {
+  getStatus(userId: number) {
     return instance.get(`profile/status/${userId}`);
   },
-  updateStatus(status) {
+  updateStatus(status: string) {
     return instance.put(`profile/status`, { status: status });
   },
-  savePhoto(photoFile) {
+  savePhoto(photoFile: string) {
     const formData = new FormData();
     formData.append("image", photoFile);
     return instance.put(`profile/photo`, formData, {
@@ -45,30 +45,62 @@ export const profileApi = {
       },
     });
   },
-  saveProfile(profile) {
+  saveProfile(profile: object) {
     return instance.put(`profile`, profile);
   },
 };
 
+export enum ResultCodesEnum {
+  Success = 0,
+  Error = 1,
+}
+
+export enum ResultCodesWithCaptcha {
+  CaptchaIsRequired = 10,
+}
+
+type authMeResponseType = {
+  data: { id: number; email: string; login: string };
+  resultCode: ResultCodesEnum;
+  messages: Array<string>;
+};
+
+type loginResponseType = {
+  data: { userId: number };
+  resultCode: ResultCodesEnum | ResultCodesWithCaptcha;
+  messages: Array<string>;
+};
+
 export const authApi = {
   authMe() {
-    return instance.get(`auth/me`);
+    return instance.get<authMeResponseType>(`auth/me`).then((res) => res.data);
   },
-  login(email, password, rememberMe = false, captcha = null) {
-    return instance.post(`auth/login`, {
-      email,
-      password,
-      rememberMe,
-      captcha,
-    });
+  login(
+    email: string,
+    password: string,
+    rememberMe = false,
+    captcha: null | string = null
+  ) {
+    return instance
+      .post<loginResponseType>(`auth/login`, {
+        email,
+        password,
+        rememberMe,
+        captcha,
+      })
+      .then((res) => res.data);
   },
   logout() {
     return instance.delete(`auth/login`);
   },
 };
 
+type getCaptchaUrlResponseType = {
+  url: string;
+};
+
 export const securityApi = {
   getCaptchaUrl() {
-    return instance.get(`security/get-captcha-url`);
+    return instance.get<getCaptchaUrlResponseType>(`security/get-captcha-url`);
   },
 };
